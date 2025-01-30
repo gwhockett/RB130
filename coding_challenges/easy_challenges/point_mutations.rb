fc8aa4bd981a7b5b2d3899e0c3533f9ae5beb6a2
gwhockett <gwhockett@gmail.com>
2025-01-30 14:35:22 -0500

    update to point_mutations.rb

--- coding_challenges/easy_challenges/point_mutations.rb
+++ coding_challenges/easy_challenges/point_mutations.rb
@@ -6,8 +6,8 @@ distance.
 The strings only contain Gs As Cs and Ts.
 What is considered a difference? Each character in each string has an index
 value starting at the left most position. If a corresponding index has
-different character value, then that is a point mutation. Point mutations
-are calculated for length of the shorter string.
+different character value, then that is a point mutation. The maximum number
+of point mutations can only be the length of the shorter string.
 Example 1:
 'GAGCCTACTAACGGGAT'
 'CATCGTAATGACGGCCT'
@@ -20,7 +20,7 @@ Zero points
 Example 3:
 'CATCGTAATGACGGCCT'
 'CATCGTAATGACGGCCT'
-No points
+Zero points
 Example 4:
 'CATCGTAATGACGGCCT'
 'TCCGGCAGTAATGCTAC'
@@ -37,36 +37,38 @@ Example 6:
  ^^^^^^^^^^^^^
 13 points
 DATA:
-input: two string objects -> constructor
-Range objects determined by the string sizes.
+We are given strings as DNA strands
+Range objects determined by the string sizes. Maybe convert to an array?
 output/return: integer
 Algorithm:
-Input two strings into a constructor
-Determine what string is the shortest
-Use the shortest index as a maximum range value
-our range object.
-create a result variable pointing to zero
-Iterate through intputs
-  Compare each character of each string at the same index location
-    If they are the same then no point value
-    If different then one point is added to result
+Input one string into a constructor and assign a variable
+Our class will be called `DNA` as shown by the given tests.
+
+Check for hamming distance
+  Input one string into a method called `hamming_distance`
+  Determine what string is the shortest
+  Use the shortest index as a maximum range value
+  our range object.
+  Create a result variable pointing to zero
+  Iterate through the range object. Each iteration will give us an index value.
+      Compare each character of each string at the same index location
+        If they are the same then no point value
+        If different then one point is added to result
 Return the result
 
 
 Code:
 =end
 class DNA
-  attr_reader :strand
-
   def initialize(strand)
     @strand = strand
   end
 
   def hamming_distance(hamming_strand)
-    shortest = [strand.size, hamming_strand.size].min
+    shortest = [@strand.size, hamming_strand.size].min
     result = 0
     (0...shortest).each do |idx|
-      result += 1 unless strand[idx] == hamming_strand[idx]
+      result += 1 unless @strand[idx] == hamming_strand[idx]
     end
     result
   end
